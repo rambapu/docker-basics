fdfacd821ef02959c4ff120ae62ab1d0cbb756a8
Ram Bapu <rambapu270197@gmail.com>
2026-05-13 16:42:02 +0530

    Implement book model and database initialization for book service

--- book-service/src/index.js
+++ book-service/src/index.js
@@ -5,19 +5,7 @@ const app = express();
 const port = 3000;
 dotenv.config();
 
-const books = [
-  { id: 1, title: "1984", author: "George Orwell" },
-  {
-    id: 2,
-    title: "Brave New World",
-    author: "Aldous Huxley",
-  },
-  {
-    id: 3,
-    title: "The Catcher in the Rye",
-    author: "J.D. Salinger",
-  },
-];
+dbStart();
 
 app.get("/books", async (req, res) => {
   res.json(books);
